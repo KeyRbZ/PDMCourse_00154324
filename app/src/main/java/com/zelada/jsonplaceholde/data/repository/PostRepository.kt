5a28cd2c9fd8c61a7c301e4ace270ad7f83fe9d3
KeyRbZ <00154324@uca.edu.sv>
2026-05-29 02:28:09 -0600

    Revert "Feat: Agregar apps de aprendizaje al curso de Kotlin"
    This reverts commit bc5c01740fd32feb0693286a60b552f15a01fb92.

--- app/src/main/java/com/zelada/jsonplaceholde/data/repository/PostRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.zelada.jsonplaceholde.data.repository
-
-import com.zelada.jsonplaceholde.domain.model.Post
-
-interface PostRepository {
-    suspend fun getPosts(): Result<List<Post>>
-    suspend fun createPost(post: Post): Result<Post>
-}
-
